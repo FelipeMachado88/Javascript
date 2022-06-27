0f927d9ba199167e4f57abc1133bc68638207cbf
FelipeMachado88 <felipedem.machado@gmail.com>
2022-06-26 22:56:57 -0300

    criando a lógica js

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ sizePassword.innerHTML = sliderElement.value;
 sliderElement.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
+
+function generatePassword(){
+    let senha = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        senha += charset.charAt(Math.floor(Math.random() * n));
+        //gerando nº inteiro com base no "charset"
+    } //lógica; passar X vezes pelo charset(quantidade de caracteres) até a seleção do usuário.
+
+    containerPassword.classList.remove("hide");
+
+    password.innerHTML = senha;
+
+    novaSenha = senha;
+}
+//id button
+
+function copyPassword(){
+    alert("Senha copiada!");
+    navigator.clipboard.writeText(novaSenha);
+} //click referenciado no "container-password"(index.html)
